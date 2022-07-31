8605425fd63e692e45508a30d0dfbe51ca7ed4cb
ArturAronov <unionco@icloud.com>
2022-07-31 16:16:27 +0800

    added custom themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
-/** @type {import('tailwindcss').Config} */ 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   plugins: [require("daisyui")],
+  daisyui: {
+    styled: true,
+    themes: ["winter", "dracula"],
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dracula",
+  },
 }
