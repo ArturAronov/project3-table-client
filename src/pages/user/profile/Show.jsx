87f5de630c5aa9e9222b830f4f4dbac4125c175c
ArturAronov <unionco@icloud.com>
2022-08-02 12:36:16 +0800

    cleaned up profile show code

--- src/pages/user/profile/Show.jsx
+++ src/pages/user/profile/Show.jsx
@@ -1,32 +1,23 @@
 import { useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+
 import TableContext from '../../../context/TableContext';
 
 const PagesUserProfileShow = () => {
-  const { data, getProfile } = useContext(TableContext);
-
-  // Turn camelCased key into Camel Cased key
-  const dataKeys = Object.keys(data).map(element => {
-    const removeCamelCase = element.split('').map(letter => {
-      return letter === letter.toUpperCase() ? ` ${letter}` : letter
-    });
-    const capitalizeFirstLetter = removeCamelCase.join('')
-    return capitalizeFirstLetter.charAt(0).toUpperCase() + capitalizeFirstLetter.slice(1);
-  });
-  const dataVals = Object.values(data);
+  const { getProfile, profileArray } = useContext(TableContext);
 
   useEffect(() => {
     getProfile();
   }, []);
+
   return (
     <div className='w-screen my-5'>
       <div className='flex justify-center py-5'>
         <div className="stats stats-vertical shadow my-5">
-        {/* Map over the data without the id */}
-          { dataKeys.splice(1, dataVals.length).map((element, index) => {
-            return (<div className="stat" key={element}>
-              <div className="flex justify-center stat-title"> {element} </div>
-              <div className="flex justify-center stat-value"> {dataVals[index+1]} </div>
+          { profileArray().map(element => {
+            return (<div className="stat" key={Object.keys(element)}>
+              <div className="flex justify-center stat-title"> {Object.keys(element)} </div>
+              <div className="flex justify-center stat-value"> {Object.values(element)} </div>
             </div>)
           })}
         </div>
