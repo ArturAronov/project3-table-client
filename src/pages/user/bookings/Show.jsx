bb73f753bcde75a8507f4920064e82db392688e7
ArturAronov <unionco@icloud.com>
2022-08-02 15:30:44 +0800

    added page title and key for map elements

--- src/pages/user/bookings/Show.jsx
+++ src/pages/user/bookings/Show.jsx
@@ -5,8 +5,9 @@ const PagesUserBookingsShow = () => {
   const { bookings } = useContext(TableContext);
   return (
     <div className='w-screen'>
+    <div className='text-center text-3xl m-5'>My Bookings</div>
       <div className="overflow-x-auto flex justify-center my-5">
-        <table className="table table-compact table-zebra">
+        <table className="table table-compact table-zebra my-5">
           <thead>
             <tr>
               <th className='text-center'>Time</th>
@@ -18,7 +19,7 @@ const PagesUserBookingsShow = () => {
           <tbody>
               {bookings.map(element => {
                 return (
-                  <tr>
+                  <tr key={element.id}>
                     <td className='text-center'>{element.time}</td>
                     <td className='text-center'>{element.dayDate}/{element.month}/{element.year}</td>
                     <td className='text-center'>{element.covers}</td>
