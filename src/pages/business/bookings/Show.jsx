1fb6a445684360e53ed99e5080968329e75dc059
ArturAronov <unionco@icloud.com>
2022-08-13 18:55:32 +0800

    beatified the code, trying to make visuals more mobile friendly

--- src/pages/business/bookings/Show.jsx
+++ src/pages/business/bookings/Show.jsx
@@ -8,13 +8,16 @@ const PagesBusinessBookingsShow = () => {
   const [ dateValue, setDateValue ] = useState(new Date());
   const [ calendarData, setCalendarData ] = useState({});
   const [ daysOpen, setDaysOpen ] = useState([]);
+  const [ dailyBookings, setDailyBookings ] = useState([]);
 
-  const filterData = () => {
-    return restaurantBookings.filter(element => {
-      if(element.year === calendarData.year && element.month === calendarData.month && element.dayDate === calendarData.date) {
+  const filterData = (day, month, year) => {
+    const bookings = restaurantBookings.filter(element => {
+      if(element.year === year && element.month === month && element.dayDate === day) {
         return element
       };
     });
+
+    setDailyBookings(bookings);
   };
 
   const bookedDays = restaurantBookings.map(element => {
@@ -24,7 +27,11 @@ const PagesBusinessBookingsShow = () => {
   useEffect(() => {
     profile?.daysOperating && setDaysOpen(profile.daysOperating.split(','));
 
-    const dateArr = moment(dateValue).format("LLLL").split(' ').map(element => element.split('').filter(letter => letter !== ',' && letter).join(''));
+    const dateArr = moment(dateValue)
+      .format("LLLL")
+      .split(' ')
+      .map(element => element.split('').filter(letter => letter !== ',' && letter)
+      .join(''));
 
     setCalendarData({
       day: dateArr[0],
@@ -32,6 +39,10 @@ const PagesBusinessBookingsShow = () => {
       month: dateArr[1],
       year: dateArr[3],
     });
+
+
+    filterData(dateArr[2], dateArr[1], dateArr[3]);
+
   }, [restaurantBookings, dateValue, profile]);
 
   return (
@@ -41,13 +52,31 @@ const PagesBusinessBookingsShow = () => {
           <Calendar
             value={dateValue}
             className='text-center'
-            tileClassName={({date, view}) => {
-              if(bookedDays.find(element => element === moment(date).format('LL'))){
-                return 'text-success font-bold bg-base-300 rounded-lg';
-              } else if(!profile?.daysOpen && !daysOpen.includes(moment(date).format('LLLL').split(',')[0])){
-                return 'text-error font-bold';
-              };
+            tileClassName={({ date }) => {
+              const dateSelected = moment(date)
+                .format('LL')
+                .split(',  ')[0];
+              const currentDate = moment(dateValue)
+                .format('LL')
+                .split(',  ')[0];
+              const isRestaurantOpenOnTheDay = !daysOpen
+                .includes(moment(date)
+                  .format('LLLL')
+                  .split(',')[0]
+                );
+
+              if(dateSelected === currentDate) {
+                return 'bg-info text-base-100 rounded-md'
+              } else if(bookedDays.find(element => element === moment(date).format('LL'))){
+                return 'text-success font-bold bg-base-300 rounded-md w-10 h-10';
+              } else if(!profile?.daysOpen && isRestaurantOpenOnTheDay){
+                return 'text-error font-bold w-10 h-10';
+              } else {
+                return 'w-10 h-10'
+              }
             }}
+
+
             onChange={value => {
               const dateArr = moment(dateValue)
                 .format("LLLL")
@@ -70,33 +99,40 @@ const PagesBusinessBookingsShow = () => {
 
         </div>
       </div>
-
+      <div className='text-center m-5 text-sm'>
+        Date Selected: {calendarData.date} / {calendarData.month} / {calendarData.year}
+      </div>
+      { dailyBookings.length ?
       <div className="overflow-x-auto my-5 flex justify-center">
         <table className="table table-zebra">
           <thead>
             <tr className='text-center'>
-              <td>Time</td>
-              <td>Date</td>
-              <td>Covers</td>
-              <td>Name</td>
-              <td></td>
+              <td className='p-3 sm:p-5'>Time</td>
+              <td className='p-3 sm:p-5'>Table</td>
+              <td className='p-3 sm:p-5'>Covers</td>
+              <td className='p-3 sm:p-5'>Name</td>
+              <td className='p-3 sm:p-5'></td>
             </tr>
           </thead>
           <tbody>
-            { filterData().map(element => {
+            { dailyBookings.map(element => {
               return (
-                <tr key={element.id}>
-                  <td>{element.time}</td>
-                  <td>{element.dayDate}/{element.month}/{element.year}</td>
-                  <td>{element.covers}</td>
-                  <td>{element.firstName} {element.lastName}</td>
-                  <td className='btn'> Info </td>
+                <tr key={element.id} className='text-center text-sm sm:text-md'>
+                  <td className='p-1 sm:p-3'>{element.time}</td>
+                  <td className='p-1 sm:p-3'>{element.tableNr}</td>
+                  <td className='p-1 sm:p-3'>{element.covers}</td>
+                  <td className='p-1 sm:p-3'>{element.firstName} {element.lastName}</td>
+                  <td className='btn mx-2'> Info </td>
                 </tr>
               )
             }) }
           </tbody>
         </table>
       </div>
+      :
+      <div className='text-center m-5 p-5'>
+        No Bookings on this day
+      </div>}
     </div>
   );
 };
