b6b25bea2c4dd43f4b3d4fa803a71c20df8ade86
ArturAronov <unionco@icloud.com>
2022-08-18 01:28:33 +0800

    removed _.omit id from profile

--- src/context/TableContext.jsx
+++ src/context/TableContext.jsx
@@ -69,7 +69,7 @@ export const TableProvider = ({ children }) => {
       .then(res => {
         dispatch({
           type: 'AUTH',
-          profile: _.omit(res.data, ['authType']),
+          profile: res.data,
           authType: res.data.authType,
           login: true,
         })
